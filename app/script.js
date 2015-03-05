fdb985d6ed77f07cae349b06863961ef0ef6e243
Daniel Goh <gohdcl@gmail.com>
2015-03-04 21:11:49 -0800

    added ajax

--- app/script.js
+++ app/script.js
@@ -1,16 +1,26 @@
 $(document).ready(
   function() {
-    var stringArray = [ "A cat!", "A dog!", "A bird!", "A penguin!" ];
+    var stringArray = [ "You get a cat!", "You get a dog!", "You get a bird!", "You get a penguin!" ];
 
     function randomString(array) {
       return array[Math.floor(Math.random() * (array.length))];
     } // end randomString
 
-  $("#generate").on("click", function() {
-        $("#string").empty(); // clear the div
+    $("button").on("click", function() {
+        var url = $(this).attr("id");
+        $.get(url, function(response) {
+            var resText;
 
+            if (typeof response === "object") {
+              resText = response.setup + " : " + response.punchline;
+            } /* end if */ else {
+              resText = response;
+            } // end else
+             $("#ajax-text").text(resText);
+            } // end function
+          ); // end $.get
         // Display a random string in the array stringArray
-        $("#string").append(randomString(stringArray));
+        // $("#string").text(randomString(stringArray));
       } // end function
     ); // end .on
   } // end wrapper function
--- server.js
+++ server.js
@@ -2,7 +2,11 @@ var express = require("express"); // import express (which is a function) from n
 var app = express(); // express() returns an object that has the functionality to create a server
 var port = process.env.PORT || 3000;
 
-var quotes = [ "I have not failed. I've just found 10,000 ways that won't work. -Thomas Edison.",  "No matter where you go, there you are. ",  "If it is a good idea, go ahead and do it. It is much easier to apologize than to get permission. - Rear Admiral Grace Hopper, USN, Ph.D" ];
+app.use(express.static(__dirname + "/app/")); // tell express to serve files from the /app folder; __dirname is the current folder
+
+var quotes = [ "I have not failed. I've just found 10,000 ways that won't work. -Thomas Edison.",
+"No matter where you go, there you are. ",
+"If it is a good idea, go ahead and do it. It is much easier to apologize than to get permission. - Rear Admiral Grace Hopper, USN, Ph.D" ];
 
 var jokes = [
   { setup: "What's the difference between a guitar and a fish?",
@@ -22,12 +26,13 @@ app.get("/joke", function(req, res) {
 
 // first parameter of .get is the endpoint, second is a function
 app.get("/", function(req, res) { // first parameter of the function is a request, second is response
-    res.send("Hello, world!");
+    res.sendFile("index.html");
   } // end function
 ); // end .get
 
 // first parameter of .get is the endpoint, second is a function
 app.get("/quote", function(req, res) { // first parameter of the function is a request, second is response
+    console.log("Someone visited quote");
     res.send(randomString(quotes));
   } // end function
 ); // end .get
